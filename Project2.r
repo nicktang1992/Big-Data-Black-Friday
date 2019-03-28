4f4e5adf59002da8dc1d27adca6e6d8ae3ab3f46
Student <student@geca.gwu.edu>
2019-03-28 16:45:38 -0400

    seperate data by occupation

--- Project2.r
+++ Project2.r
@@ -4,10 +4,28 @@ BF
 BF['User_ID']
 summary(BF)
 
+un <- unique(BF['Occupation'])
+sort(un$Occupation)
+
 BF[which(BF$Product_ID == 'P00069042'),]
+BF[which(BF$Occupation == 1),]
+
+sapply(BF, mode)
+
+#cleaning data for clustering (replace them to numeric data and normalize using min-max)
+#1. gender
+#2. age 
+#3. stay in current city
 
 #create subset of data rows by:
 #1. occupation
+occupation_df <- list()
+for(i in 1:20) {
+  occupation_df[[i]] = BF[which(BF$Occupation==i),]
+  
+}
+occupation_df[1]
+
 #2. gender
 #3. product_id?
 
